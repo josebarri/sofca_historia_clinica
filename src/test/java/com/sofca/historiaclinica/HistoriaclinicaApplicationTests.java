8d94c7f0ef2f28e2c0fa4d14ec7ef3915938c274
Jose Barrios <josealfredobarriossalado@gmail.com>
2024-08-20 10:40:17 -0500

    first commit

---
+++ src/test/java/com/sofca/historiaclinica/HistoriaclinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofca.historiaclinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoriaclinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
